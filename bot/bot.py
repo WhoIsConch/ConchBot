ff6a6d91bde8935ce21f431532080fe042c05aeb
pusheen3 <64660183+Jerry-py@users.noreply.github.com>
2021-05-16 16:23:45 -0400

    Changed loading cog way. Also deleted sqlite db prefixes gonna work on later

--- bot/bot.py
+++ bot/bot.py
@@ -13,17 +13,12 @@ from bot.cogs.utils import errors
 load_env = load_dotenv()
 
 
-
 def prefix(bot, message):
-    prefixes = aiosqlite.connect('db/config.db')
-    cursor = prefixes.cursor()
-    cursor.execute(f"SELECT prefix from prefixes WHERE guildid = '{message.guild.id}'")
-    result = cursor.fetchone()
-    prefix = str(result[0])
     user_id = bot.user.id
     base = [f'<@!{user_id}> ', f'<@{user_id}> ', 'cb ', 'Cb ', 'CB ', 'cB ']
     base.append(prefix)
     return base
+
 
 class Client(commands.Bot):
     def __init__(self):
@@ -40,31 +35,30 @@ class Client(commands.Bot):
         super().__init__(command_prefix=prefix, intents=intents, allowed_mentions=allowed_mentions)
     
 
-    def load_cogs(self):
-        if discord.ext.commands.errors.ExtensionAlreadyLoaded:
-            pass
-        else:
-            print("Loading All cogs...")
-            print("------")
-            for filename in os.listdir(f"./bot/cogs"):
-                if filename.endswith(f".py"):
-                    self.load_extension(f"bot.cogs.{filename[:-3]}")
-                    print(f"Loaded `{filename[:20]}` Cog")
-            self.remove_command('help')
 
+
+    def load_cogs(self):
+        self.remove_command('help')
+        print("Loading All cogs...")
+        print("------")
+        for filename in os.listdir(f"./bot/cogs"):
+            if filename.endswith(f".py"):
+                self.load_extension(f"bot.cogs.{filename[:-3]}")
+                print(f"Loaded `{filename[:20]}` Cog")
+        print("------")
+        self.load_extension('jishaku')
+        print("Loaded `jishaku`")
+            
     def on_ready(self):
         print("ConchBot is online!")
 
     
 
-
-    def setup(self):
-        self.load_cogs()
-        self.on_ready()
         
 
     def run(self):
-        self.setup()
+        self.load_cogs()
+        self.on_ready()
 
         TOKEN = os.getenv("TOKEN")
         

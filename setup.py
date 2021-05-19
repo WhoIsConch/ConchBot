9c65d138477a12c88dbbfa28bc1bc4e59fc517b1
pusheen3 <64660183+Jerry-py@users.noreply.github.com>
2021-05-19 18:55:01 -0400

    Simple mistake

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ if not os.path.exists('.env'):
     print("------")
     print("To get the github repository branch go to https://github.com and create or use existing a repository. This is for refresh command. Note: Private repository might not work")
     print("To get branch is where it says branch and a number if you don't see it create file or upload the code to the repo and it should appear and click on it. The default should be `main`")
-    print("------"
+    print("------")
     github_repo_branch = input("Your Github Repository Name: ")
     env.write(f"GITHUB_REPO_BRANCH={github_repo_branch}\n")
     print("------")

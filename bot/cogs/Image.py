75b0495f1aee959b5ea274c91e112d7c7869240e
pusheen3 <64660183+Jerry-py@users.noreply.github.com>
2021-05-20 12:30:28 -0400

    Test 4?

--- bot/cogs/Image.py
+++ bot/cogs/Image.py
@@ -59,17 +59,13 @@ class Image(commands.Cog):
             draw.text((153, 535), val1, font = font, fill=fill_color, stroke_width=2, stroke_fill=stroke_color)
         img.save("text.png")
         await ctx.send(file = discord.File("text.png"))
-        file = 'text.png'
-        location = "./"
-        path = os.path.join(location, file)
-        os.remove(path)
 
     @commands.command()
     @commands.cooldown(1, 10, commands.BucketType.user) 
     async def brain(self, ctx, *, content):
         msg = await ctx.send("Creating your meme...")
-        img = PIL.Image.open("./bot/src/MemeTemplates/Brain.png")
-        font = ImageFont.truetype("./bot/src/arial.ttf", 10)
+        img = PIL.Image.open("bot/src/MemeTemplates/Brain.png")
+        font = ImageFont.truetype("bot/src/arial.ttf", 10)
         draw = ImageDraw.Draw(img)
         text = textwrap.fill(content, width=25)
         draw.text((17, 176), text, font=font, fill="Black")

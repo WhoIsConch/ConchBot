dff0cfaa964fb9ad30d5e03b7551d17df2f62061
pusheen3 <64660183+Jerry-py@users.noreply.github.com>
2021-05-20 09:24:46 -0400

    IDK

--- bot/cogs/utils/handler.py
+++ bot/cogs/utils/handler.py
@@ -15,72 +15,81 @@ class CommandErrorHandler(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        if isinstance(error, commands.CommandNotFound):
-            await ctx.send(f"Command: `{ctx.command.name}` doesn't exist")
+        if commands.CommandNotFound:
+            await ctx.send(f"Command: `{ctx.command}` doesn't exist")
+            return
 
 
-        if isinstance(error, discord.errors.HTTPException):
-            await ctx.send("Something went wrong. We'll report it. Note: The bot might be ratelimited")
-            now = datetime.datetime.now()
-            time = datetime.time(hour=now.hour, minute=now.minute).isoformat(timespec='minutes')
-            error_channel = self.client.get_channel(int(os.getenv("ERROR_CHANNEL")))
-            await error_channel.send(f'Error Occured at {time} and in {ctx.guild.name} by {ctx.author.name}#{ctx.author.discriminator} with the command `{ctx.command.name}`: ``` {error} ```')
+        if discord.errors.HTTPException:
+            await ctx.send("Something went wrong. Note: The bot might be ratelimited")
+            return
 
         if isinstance(error, commands.DisabledCommand):
-            await ctx.send(f'{ctx.command} has been disabled.')
+            await ctx.send(f'The command has been disabled.')
+            return
         
         if isinstance(error, commands.CommandOnCooldown):
-            await ctx.send(f"You are cooldown for the command `{ctx.command.name}`. Please try again in {error.retry_after:.2f}s")
+            await ctx.send(f"You are cooldown. Please try again in {error.retry_after:.2f}s")
+            return
         
         if isinstance(error, discord.ext.commands.errors.NotOwner):
             await ctx.send("You are not the owner of this bot so you can't use this command")
+            return
 
         if IndexError:
             await ctx.send("Thats not a valid number choice")
+            return
 
         if ValueError:
             await ctx.send("You need to tell me what I need to do, ig this is a image, separate text on the top and bottom with a comma.")
+            return
 
-        if isinstance(error, ValueError):
-            await ctx.send("That isn't a valid number or text.")
 
         if isinstance(error, commands.MissingPermissions):
             await ctx.send("You don't have the permissions to do that! Please contact a server admin to do that for you.")
+            return
 
         if isinstance(error, commands.ChannelNotFound):
             await ctx.send("Channel not found.")
-
+            return
         
         
         if isinstance(error, commands.MemberNotFound):
             await ctx.send("That member doesn't exist.")
+            return
 
         if isinstance(error, discord.NotFound):
             await ctx.send("Couldn't find that sorry")
+            return
 
         if isinstance(error, asyncio.TimeoutError):
             await ctx.send("You waited too long :(")
-        
+            return
+
         if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
             await ctx.send("There are required arguements/parameters you need to input")
-        
+            return
 
         elif isinstance(error, commands.NoPrivateMessage):
             try:
                 await ctx.author.send(f'{ctx.command} can not be used in Private Messages.')
+                return
             except discord.HTTPException:
                 await ctx.send("Something went wrong. We'll report it. Note: The bot might be ratelimited")
                 now = datetime.datetime.now()
                 time = datetime.time(hour=now.hour, minute=now.minute).isoformat(timespec='minutes')
                 error_channel = self.client.get_channel(int(os.getenv("ERROR_CHANNEL")))
                 await error_channel.send(f'Error Occured at {time} and in {ctx.guild.name} by {ctx.author.name}#{ctx.author.discriminator} with the command `{ctx.command.name}`: ``` {error} ```')
+                return
         
 
         elif isinstance(error, commands.BadArgument):
             if ctx.command.qualified_name == 'tag list':
                 await ctx.send('I could not find that member. Please try again.')
+                return
             else:
                 await ctx.send("You were supposed to type that but you ended typing that")
+                return
         
 
 
@@ -89,7 +98,7 @@ class CommandErrorHandler(commands.Cog):
             time = datetime.time(hour=now.hour, minute=now.minute).isoformat(timespec='minutes')
             error_channel = self.client.get_channel(int(os.getenv("ERROR_CHANNEL")))
             await error_channel.send(f'Error Occured at {time} and in {ctx.guild.name} by {ctx.author.name}#{ctx.author.discriminator} with the command `{ctx.command.name}`: ``` {error} ```')
-            
+            return
 
 def setup(client):
     client.add_cog(CommandErrorHandler(client))

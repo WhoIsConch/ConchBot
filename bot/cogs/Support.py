0ee26cd032890ac4121dda966f1b1848139b18f3
pusheen3 <64660183+Jerry-py@users.noreply.github.com>
2021-05-20 12:53:28 -0400

    Undo

--- bot/cogs/Support.py
+++ bot/cogs/Support.py
@@ -27,7 +27,7 @@ class Support(commands.Cog):
     
     @commands.command()
     @commands.cooldown(1, 86400, commands.BucketType.user)
-    async def report(self, ctx, bug="A Error"):
+    async def report(self, ctx, bug):
         channel = self.client.get_channel(795711741606101024)
         db = await aiosqlite.connect('./bot/db/config.db')
         cursor = await db.cursor()

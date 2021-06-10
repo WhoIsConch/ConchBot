9c949cdeb9e736ac4022ca1733daafb37e212bf2
pusheen3 <64660183+Jerry-py@users.noreply.github.com>
2021-06-10 12:41:26 -0400

    Nah

--- launcher.py
+++ launcher.py
@@ -1,5 +1,4 @@
 from bot.bot import ConchBot
-import asyncio
 from web.main import flask_thread, run
 import time
 
@@ -11,10 +10,4 @@ def run_web():
     run()
 
 if __name__ == "__main__":
-    flask_thread(func=run)
-    print("------")
-    time.sleep(1)
-    print("------")
-    print("Loaded Website")
-    print("------")
     run_bot()

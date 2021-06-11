90b055fdc4cea7d61613c40667c4aed52493db34
pusheen3 <64660183+Jerry-py@users.noreply.github.com>
2021-06-11 09:43:57 -0400

    Mistake

--- launcher.py
+++ launcher.py
@@ -1,13 +1,9 @@
 from bot.bot import ConchBot
-from web.main import flask_thread, run
-import time
 
 def run_bot():
     bot = ConchBot()
     bot.run()
 
-def run_web():
-    run()
 
 if __name__ == "__main__":
     run_bot()

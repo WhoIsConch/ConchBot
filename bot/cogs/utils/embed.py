ca1f21c70a4f9a7a519f5189d74149a530c46989
pusheen3 <64660183+Jerry-py@users.noreply.github.com>
2021-09-23 21:05:22 -0400

    Added values and updated requirements.txt
    Added ffmpeg in there

--- bot/cogs/utils/embed.py
+++ bot/cogs/utils/embed.py
@@ -13,19 +13,19 @@ class Embeds:
     ]
         self.time = datetime.datetime.utcnow().strftime('%Y:%m:%d %H:%M:%S')
 
-    def OnError(self, command_name, time, *, reason : str):
+    def OnError(self, command_name : str, time : str, *, reason : str):
         self.Embed = discord.Embed(title="Oh no an error occurred", color=discord.Color.red())
         self.Embed.add_field(name="Command Name: ", value=command_name)
         self.Embed.add_field(name="At: ", value=time)
         self.Embed.add_field(name="Reason", value=reason)
         return self.Embed
 
-    def OnCooldown(self, *, ctx, error):
+    def OnCooldown(self, *, ctx=commands.Context, error : str):
         self.cooldown_name = random.choice(self.cooldown_choices)
         self.Embed = discord.Embed(title=self.cooldown_name, description="You need to slow down and don't spam the bot\n Retry after {:.2f}".format(ctx.command.name, error.retry_after), color=discord.Color.blue())
         return self.Embed
 
-    def OnApiError(self, *, command_name, status):
+    def OnApiError(self, *, command_name : str, status : int):
         self.Embed = discord.Embed(title="Oh no an error occurred", color=discord.Color.red(), description="Sorry but something went wrong. DM Jerry.py#4249 if it keeps happening")
         self.Embed.add_field(name="Command Name: ", value=command_name)
         self.Embed.add_field(name="At: ", value=self.time)
